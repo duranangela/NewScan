f5b2d005a407e14f2fb2affe2dc95ee561281675
Angela Duran <duranangela19@gmail.com>
2018-11-18 11:33:44 -0700

    Add article service to make api call

---
+++ app/services/article_service.rb
@@ -0,0 +1,34 @@
+require 'date'
+
+class ArticleService
+
+  def initialize
+  end
+
+  def articles
+    articles = JSON.parse(response.body, symbolize_names: true)[:articles]
+    articles = articles.map do |article|
+      article = [article[:title], article[:source][:name], article[:url]]
+      article
+    end
+    articles
+  end
+
+  def response
+    Faraday.get("https://newsapi.org/v2/everything", params)
+  end
+
+  def params
+    {
+      q: 'election',
+      sortBy: 'publishedAt',
+      from: date,
+      apiKey: ENV['API_KEY']
+    }
+  end
+
+  def date
+    Date.today - 3
+  end
+
+end

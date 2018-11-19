462254ea9ff422d51519428bb8b550387f709035
Angela Duran <duranangela19@gmail.com>
2018-11-19 16:30:46 -0700

    Add topic and date input form

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
 
   def index
-    @presenter = ArticleService.new
+    @topic = params[:topic] || ""
+    if (params[:from_date] == '') || (!params[:from_date])
+      params[:from_date] = (Date.today - 3)
+    end
+    if (params[:to_date] == '') || (!params[:to_date])
+      params[:to_date] = Date.today
+    end
+    @from_date = params[:from_date] || ""
+    @to_date = params[:to_date] || ""
+    @presenter = ArticleService.new(params[:topic], @from_date, @to_date)
   end
 
 end
